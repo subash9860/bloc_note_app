cc54ece85cbeb40fa1768a178e82f776ca6f6736
subash kc <subaazkc@gmail.com>
2022-05-25 23:00:45 +0545

    first commit

---
+++ lib/strings.dart
@@ -0,0 +1,17 @@
+const emailOrPasswordEmptyDialogTitle =
+    'Please fill in both email and the password fileds';
+const emailOrPasswordEmptyDescriptor =
+    'You seem to have forgetten to enter either the emial or the password field, or both. please enter.';
+
+const loginErrorDialogTitle = 'Login error';
+
+const loginErrorDialogContent =
+    "Invalid email/password combination. Please try again with valid login credentials.";
+
+const pleaseWait = "Please wait...";
+
+const enterYourPasswordHere = "Enter your password here..";
+const enterYourEmailHere = "Enter your email here...";
+const ok = 'ok';
+const login = "Log in";
+const homePage = 'Home Page';
